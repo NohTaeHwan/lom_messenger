8e48f1c2136270cd8d3ccde8485fedb978afba7e
김준형 <cosyq1305@lastonemile.co.kr>
2022-01-19 21:33:46 +0900

    first commit

---
+++ src/main/java/com/example/lom_messenger/LomMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lom_messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LomMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LomMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lom_messenger/LomMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lom_messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LomMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
