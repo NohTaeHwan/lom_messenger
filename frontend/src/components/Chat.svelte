f4321d09721b27e026e5c24616c8da21fc16e6a9
San-Goon <tigerlove95@naver.com>
2022-02-03 22:40:11 +0900

    채팅방 대충 구현

---
+++ frontend/src/components/Chat.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { chats } from "../store";
+
+    let chat = ""
+
+    const sendChat = () => {
+        if (!chat || !chat.trim()) {
+            return
+        }
+        $chats.push({ chat })
+        $chats = $chats
+        chat = ""
+    }
+</script>
+
+<div>
+    <div class="chatList" style="height: 800px">
+    {#each $chats as chatList}
+        <div>{chatList.chat}</div>
+    {/each}
+    </div>
+    <div class="input1" style="flex-grow: 1; display: flex;">
+        <input
+            bind:value={chat}
+            type="text"
+            class="input2"
+            style="flex-grow: 1;"
+            on:keyup={ e => {
+              if (e.key === "Enter") sendChat()
+            }} />
+        <button
+            on:click={sendChat}
+            style="margin-left: 20px"
+        >
+            Send
+        </button>
+    </div>
+</div>
+
+<style lang="css">
+
+</style>
