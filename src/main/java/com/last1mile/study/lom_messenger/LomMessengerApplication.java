378402587f9cb506966865fa57a9d136383b68fa
노태환 <thnoh93@lastonemile.co.kr>
2022-02-03 14:02:52 +0900

    datasource 제외해서 실행하도록 임시 수정

--- src/main/java/com/last1mile/study/lom_messenger/LomMessengerApplication.java
+++ src/main/java/com/last1mile/study/lom_messenger/LomMessengerApplication.java
@@ -1,9 +1,12 @@
 package com.last1mile.study.lom_messenger;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class LomMessengerApplication {
 
     public static void main(String[] args) {
